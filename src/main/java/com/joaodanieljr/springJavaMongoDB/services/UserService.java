a2816334903fe584fc6db1f45e40c4b6ae9db79f
joaodanieljr <joaodanieljr@gmail.com>
2021-01-27 20:44:45 -0300

    Usuario por id + Tratamento 404

--- src/main/java/com/joaodanieljr/springJavaMongoDB/services/UserService.java
+++ src/main/java/com/joaodanieljr/springJavaMongoDB/services/UserService.java
@@ -1,11 +1,13 @@
 package com.joaodanieljr.springJavaMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaodanieljr.springJavaMongoDB.domain.User;
+import com.joaodanieljr.springJavaMongoDB.exceptions.ObjectNotFoundException;
 import com.joaodanieljr.springJavaMongoDB.repository.UserRepository;
 
 @Service
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() ->  new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
