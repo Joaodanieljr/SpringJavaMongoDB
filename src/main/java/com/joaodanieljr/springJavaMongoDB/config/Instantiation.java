81bea13d9622ef77aa4aa8c0a59b44ab6d503d4f
joaodanieljr <joaodanieljr@gmail.com>
2021-01-27 23:43:13 -0300

    Criacao AuthorDTO

--- src/main/java/com/joaodanieljr/springJavaMongoDB/config/Instantiation.java
+++ src/main/java/com/joaodanieljr/springJavaMongoDB/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.joaodanieljr.springJavaMongoDB.domain.Post;
 import com.joaodanieljr.springJavaMongoDB.domain.User;
+import com.joaodanieljr.springJavaMongoDB.dto.AuthorDTO;
 import com.joaodanieljr.springJavaMongoDB.repository.PostRepository;
 import com.joaodanieljr.springJavaMongoDB.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User jose = new User(null, "Jose", "jose@gmail.com");
 		User joao = new User(null, "Joao", "joao@gmail.com");
 		
-	
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, jose, joao));
+	
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO( maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
